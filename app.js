bd41c6644984ba20925ff4124877b7ab35737586
Osarenoma Blessing Dick <blessingosarenoma@gmail.com>
2025-09-14 16:57:52 +0100

    images

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const images = [
   },
   {
     src: "https://images.unsplash.com/photo-1503656142023-618e7d1f435a?q=80&w=1589&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    alt: "two zebra",
+    alt: "two zebras",
   },
   {
     src: "https://images.unsplash.com/flagged/photo-1557650454-65194af63bf9?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -30,6 +30,14 @@ const images = [
     src: "https://images.unsplash.com/photo-1696772686504-09010046b1d3?q=80&w=2065&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     alt: "sea lion",
   },
+  {
+    src: "https://images.unsplash.com/photo-1471005197911-88e9d4a7834d?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    alt: "crocodile",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1505259839540-04105fcd8ba3?q=80&w=1094&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    alt: "redfox",
+  },
 ];
 
 function createThumbnails() {
@@ -38,7 +46,8 @@ function createThumbnails() {
     console.log(image);
     imageElement.src = image.src;
     imageElement.addEventListener("click", function () {
-      console.log(image);
+      console.log("image");
+      createBigImage(image);
     });
 
     thumbnails.appendChild(imageElement);
@@ -46,7 +55,7 @@ function createThumbnails() {
 }
 createThumbnails();
 function createBigImage(imgDetails) {
-  displayContainer.innerHTML = "";
+  displayContainer.innerHTML = " ";
   const bigImage = document.createElement("img");
   bigImage.src = imgDetails.src;
   bigImage.alt = imgDetails.alt;
